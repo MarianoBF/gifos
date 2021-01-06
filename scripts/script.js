463b067d4f0fb35e21688bbf274192c0df0c571a
MarianoBF <mariano.b.fernandez@gmail.com>
2021-01-05 21:23:58 -0300

    Agregado trending y reajustado calesita

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,13 @@
-
-  // Menú desplegable
+/*********************************
+MENÚ DESPLEGABLE
+*********************************/
 
   function openNav() {
     document.getElementById("menu").style.width = "100%";
     document.getElementById("abrirmenu").style.display = "none";
     document.getElementById("cerrarmenu").style.display = "inline";
   }
-
-  
+ 
   function closeNav() {
     document.getElementById("menu").style.width = "0%";
     document.getElementById("abrirmenu").style.display = "inline";
@@ -15,87 +15,56 @@
   }
 
 
-// Barra búsqueda
-
-// document.getElementById("iconoBusqueda").style.background = "none";
-// document.getElementById("barraBusqueda").style.paddingLeft = "15%";
-// document.getElementById("iconoBusqueda").style.background = "#FFF url(/images/close.svg) 0 0 no-repeat";
-
-
-
+/*********************************
+BARRA BÚSQUEDA
+*********************************/
 function buscar() {
- 
 
-   var query = document.querySelector(".busqueda").value; 
-   
-   alert(query)
+event.preventDefault();
 
-  }
+var busqueda = document.querySelector(".busqueda").value; 
 
+document.getElementById("resultadosBusqueda").style.display = "block";
+document.getElementById("tituloBusqueda").innerHTML = busqueda
 
+let vaciar = document.getElementById("contenedorBusqueda");
+vaciar.textContent = "";
 
-// function getUserInput() { 
-//     var inputValue = document 
-//       .querySelector(".busqueda").value; 
-//     return inputValue; 
-//   }
+function mostrarGiphy (gif) {
+  const imagen = document.createElement('IMG')
+  imagen.src = gif.images.downsized.url
+  document.getElementById("contenedorBusqueda").appendChild(imagen);
+}
 
-//   document.querySelector(".buscar").addEventListener("click", function () { 
-//     var inputValue = document 
-//       .querySelector(".busqueda").value; 
-//     var userInput = getUserInput(); 
-//     searchGiphy(userInput); 
-//   });
+const urlbusqueda = "https://api.giphy.com/v1/gifs/search?" 
++"api_key=uTnjhcYC0B52sTn6MzoPXGkdJ6yxZgYQ"
++"&q="
++busqueda
++"&lang=es"
++"&limit=3"
 
+fetch(urlbusqueda)
+  .then(resultado => resultado.json())
+  .then(({ data }) =>  data.map(mostrarGiphy));
+}
 
-//   function searchGiphy(searchQuery) { 
-//     var url = 
-//   "https://api.giphy.com/v1/gifs/search?api_key=uTnjhcYC0B52sTn6MzoPXGkdJ6yxZgYQ&q="
-//     + searchQuery +"&limit=5&lang=es"; 
-        
-//     // AJAX Request 
-      
-//     var GiphyAJAXCall = new XMLHttpRequest(); 
-//     GiphyAJAXCall.open("GET", url); 
-//     GiphyAJAXCall.send(); 
-    
-//     GiphyAJAXCall.addEventListener("load", function (data) { 
-//       var actualData = data.target.response; 
-//       pushToDOM(actualData); 
-//       console.log(actualData); 
-        
-//     }); 
-//   } 
+/*********************************
+TRENDING
+*********************************/
 
+const urltrending = "https://api.giphy.com/v1/gifs/trending?" 
++"api_key=uTnjhcYC0B52sTn6MzoPXGkdJ6yxZgYQ"
++"&limit=12"
 
-//  function pushToDOM(response) { 
-  
-//     // Turn response into real JavaScript object 
-//     response = JSON.parse(response); 
-      
-//     // Drill down to the data array 
-//     var images = response.data; 
-    
-//     // Find the container to hold the response in DOM 
-//     var container = document.querySelector(".display"); 
-      
-//     // Clear the old content since this function  
-//     // will be used on every search that we want 
-//     // to reset the div 
-//     container.innerHTML = ""; 
-    
-//     // Loop through data array and add IMG html 
-//     images.forEach(function (image) { 
-    
-//       // Find image src 
-//       var src = image.images.fixed_height.url; 
-    
-//       // Concatenate a new IMG tag 
-//       container.innerHTML += "<img src='" 
-//         + src + "' class='container-image' />"; 
-//     }); 
-//   }
+fetch(urltrending)
+  .then(res => res.json())
+  .then(({ data }) => data.map(mostrarTrending))
 
+function mostrarTrending (gif) {
+  const imagen = document.createElement('IMG')
+  imagen.src = gif.images.original.url
+  document.getElementById("contenedorTrending").appendChild(imagen);
+}
 
 /*********************************
 CALESITA
@@ -107,7 +76,7 @@ var runSlider;
 function Carousel() {
 clearTimeout(runSlider);
     firstval += 2;
-    parent = document.getElementById('imagenesCalesita');
+    parent = document.getElementById('contenedorTrending');
     parent.style.left = "-" + firstval + "px";
     if (!(firstval % 180)) {
         setTimeout(Carousel, 3000);
