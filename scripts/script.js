08d9a6d01965dee524f268079373319a1a356f56
MarianoBF <mariano.b.fernandez@gmail.com>
2021-01-07 14:58:45 -0300

    agregar JS sugerencias, temas trending, ajustes html

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,14 @@
+
+/*
+1-MENÚ DESPLEGABLE
+2-BARRA BÚSQUEDA (SUGERENCIAS Y RESULTADOS)
+3-TRENDING (TEXTO Y GIFS)
+4-CALESITA
+*/
+
+
 /*********************************
-MENÚ DESPLEGABLE
+1-MENÚ DESPLEGABLE
 *********************************/
 
   function openNav() {
@@ -16,8 +25,33 @@ MENÚ DESPLEGABLE
 
 
 /*********************************
-BARRA BÚSQUEDA
+2-BARRA BÚSQUEDA (SUGERENCIAS Y RESULTADOS)
 *********************************/
+
+function sugerencias() {
+
+let sugerencia = document.querySelector(".busqueda").value; 
+
+if (sugerencia.length > 2) {
+
+let urlSugerencias = "https://api.giphy.com/v1/tags/related/" 
++sugerencia
++"?api_key=uTnjhcYC0B52sTn6MzoPXGkdJ6yxZgYQ"
+
+fetch(urlSugerencias)
+  .then(res => res.json())
+  .then(({ data }) => data.map(mostrarSugerencias))
+
+function mostrarSugerencias (sug) {
+
+let opcionSug = document.createElement("option"); 
+var valorSug = document.createTextNode(sug.name); 
+  opcionSug.appendChild(valorSug); 
+  document.getElementById("busquedasSugeridas").appendChild(opcionSug); 
+
+};}
+}
+
 function buscar() {
 
 event.preventDefault();
@@ -31,44 +65,59 @@ let vaciar = document.getElementById("contenedorBusqueda");
 vaciar.textContent = "";
 
 function mostrarGiphy (gif) {
-  const imagen = document.createElement('IMG')
+  let imagen = document.createElement('IMG')
   imagen.src = gif.images.preview_gif.url
   document.getElementById("contenedorBusqueda").appendChild(imagen);
 }
 
-const urlbusqueda = "https://api.giphy.com/v1/gifs/search?" 
+let urlBusqueda = "https://api.giphy.com/v1/gifs/search?" 
 +"api_key=uTnjhcYC0B52sTn6MzoPXGkdJ6yxZgYQ"
 +"&q="
 +busqueda
 +"&lang=es"
 +"&limit=12"
 
-fetch(urlbusqueda)
+fetch(urlBusqueda)
   .then(resultado => resultado.json())
   .then(({ data }) =>  data.map(mostrarGiphy));
   
 }
 
 /*********************************
-TRENDING
+3- TRENDING (TEXTO Y GIFS)
 *********************************/
 
-const urltrending = "https://api.giphy.com/v1/gifs/trending?" 
+let urlTrending = "https://api.giphy.com/v1/gifs/trending?" 
 +"api_key=uTnjhcYC0B52sTn6MzoPXGkdJ6yxZgYQ"
 +"&limit=12"
 
-fetch(urltrending)
+fetch(urlTrending)
   .then(res => res.json())
   .then(({ data }) => data.map(mostrarTrending))
 
 function mostrarTrending (gif) {
-  const imagen = document.createElement('IMG')
+  let imagen = document.createElement('IMG')
   imagen.src = gif.images.preview_gif.url
   document.getElementById("contenedorTrending").appendChild(imagen);
 }
 
+let urlTemasTrending = "https://api.giphy.com/v1/trending/searches?" 
++"api_key=uTnjhcYC0B52sTn6MzoPXGkdJ6yxZgYQ"
+
+fetch(urlTemasTrending)
+  .then(res => res.json())
+  .then(({ data }) => data.map(mostrarTemasTrending))
+
+function mostrarTemasTrending (tema) {
+  tema = mayusculizar(tema)
+  document.getElementById("temasTrending").innerHTML += tema + " - " 
+  
+}
+
+
+
 /*********************************
-CALESITA
+4-CALESITA
 *********************************/
 
 var firstval = 0;
@@ -123,3 +172,14 @@ Carousel();
 //     }
 //     runSlider = setTimeout(rightClick, 10);
 // }
+
+
+/********************
+5-AUX
+*******************/
+
+let mayusculizar = (palabras) => {
+  palabras = palabras.split(" ");
+  palabras = palabras.map((word, index) => word[0].toUpperCase() + word.slice(1));
+  palabras = palabras.join(" ")
+  return palabras }
